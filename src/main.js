44b6b8899b91ea0003e7a57c3c7f866990aa3904
Isaac E. <122501927+IsaacUtah1379@users.noreply.github.com>
2025-08-14 17:44:48 -0600

    Actually fix the file path

--- src/main.js
+++ src/main.js
@@ -68,7 +68,7 @@ function analyze(event, params) {
 
         fs.writeFileSync(configPath, tomlString);
 
-        let exePath = path.join(app.getAppPath(), 'executables', 'tier_analysis.exe');
+        let exePath = path.join(path.dirname(app.getPath('exe')), 'executables', 'tier_analysis.exe');
 
         let tierProc = childProc.spawn(exePath, [path.basename(params.inputFile), configPath, params.outputFolder], { cwd: path.dirname(params.inputFile) });
         childProcCount++;
